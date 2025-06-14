509f2d4f5ae2a852a0872fe538198eefb62d0422
Huyen Pham <huyen.pham@ascension6.com.au>
2025-06-14 13:08:55 +0700

    add component icons

---
+++ components/Icons/IconArrowTop.tsx
@@ -0,0 +1,12 @@
+export const IconArrowTop = () => {
+    return (
+		<svg fill="currentColor" width="24px" height="24px" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
+			<g>
+				<path
+					d="M960.406 0 345 615.176l81.364 81.366L902.83 220.075V1920h114.922V220.075l476.466 476.467 81.366-81.366z"
+					fillRule="evenodd"
+				></path>
+			</g>
+		</svg>
+	);
+};
---
+++ components/Icons/PseudoCorner.tsx
@@ -0,0 +1,14 @@
+export const PseudoCorner = () => {
+	return (
+		<svg
+			className="pseudo-corner"
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 12 12"
+			aria-hidden="true"
+			role="presentation"
+		>
+			<path fill="currentColor" d="M12 12C12 5.373 6.627 0 0 0h12v12Z"></path>
+		</svg>
+	);
+};
