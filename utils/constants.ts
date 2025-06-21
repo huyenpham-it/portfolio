1e24d21531288ad6652caa33f8cbdb36bf73bb37
Huyen Pham <huyen.pham@ascension6.com.au>
2025-06-21 22:57:50 +0700

    add constants and variables

---
+++ utils/constants.ts
@@ -0,0 +1,124 @@
+export const websiteProjects = [
+	{
+		type: 'website',
+		title: 'BackMarket',
+		description: 'E-commerce website for selling refurbished products',
+		detail: '_Technologies: PHP (Symfony / Ec-cube), MySQL, NodeJS, ReactJS, Redis, AWS.\n_Team size: 6.',
+		notice: 'The Korean market website has closed due to reduced revenue. You can refer to the Japanese market website at "backmarket.co.jp". It is almost the same in terms of UI/UX.',
+		image: '/backmarket.png',
+		link: 'https://backmarket.co.kr/'
+	},
+	{
+		type: 'website',
+		title: 'Ho Tram',
+		description: `Travel website introducing Saigon's beach paradise`,
+		detail: '_Technologies: PHP (Wordpress), MySQL, SiteGround.\n_Team size: 3.',
+		notice: '',
+		image: '/hotram.png',
+		link: 'https://hotrambeach-vietnam.com/'
+	},
+	{
+		type: 'website',
+		title: 'Aka-iki',
+		description: 'E-commerce website for selling custom hoodies',
+		detail: '_Technologies: Shopify, MySQL, ReactJS, NodeJS, Digital Ocean.\n_Team size: 4.',
+		notice: '',
+		image: '/aka-iki.png',
+		link: 'https://aka-iki.com/'
+	},
+	{
+		type: 'website',
+		title: 'Ascension 6',
+		description: 'Website providing information',
+		detail: '_Technologies: PHP (Wordpress), MySQL, SiteGround.\n_Team size: 2.',
+		notice: '',
+		image: '/ascension.png',
+		link: 'https://ascension6.com.au/'
+	}
+];
+
+export const designProjects = [
+	{
+		type: 'design',
+		title: 'Logo Design',
+		description: 'This is my design using canva',
+		image: '/logo.png'
+	},
+	{
+		type: 'design',
+		title: 'Logo Design',
+		description: 'This is my design using canva',
+		image: '/logo-1.png'
+	},
+	{
+		type: 'design',
+		title: 'Anh Chuyen',
+		description: `Banner for swimming pool - playground - coffee complex`,
+		image: '/banner-1.jpg'
+	},
+	{
+		type: 'design',
+		title: 'Anh Chuyen',
+		description: `Banner for swimming pool - playground - coffee complex`,
+		image: '/banner-2.jpg'
+	},
+	{
+		type: 'design',
+		title: 'Anh Chuyen',
+		description: 'Customer Note',
+		image: '/gop-y.png'
+	}
+];
+
+export const experiences = [
+	{
+		time: '2015 - 2022',
+		title: 'Computer Network and Communication',
+		place: 'University of Information Technology - VNUHCM',
+		content: '_GPA: 7.57 / 10.00\n_TOEIC - 2020: 645'
+	},
+	{
+		time: '2022 - 2025',
+		title: 'Front-End Developer',
+		place: 'TKG VIETNAM - Ascension 6',
+		content: `_Writing clean, efficient, and maintainable code.
+                _Apply the new UI using HTML, CSS (SCSS), and JavaScript (jQuery / ReactJS).
+                _Making sure web pages look good on all devices using CSS frameworks(Bootstrap, Tailwind CSS) and media queries.
+                _Enhancing page speed and performance by using lazy loading, optimizing images, and reducing unnecessary scripts.
+                _Ensuring the website functions correctly across different browsers like Chrome, Safari, ...
+                _Implementing designs provided by designers while ensuring the best user experience.
+                _Working with APIs (RESTful APIs, GraphQL).
+                _Using Git and GitHub (experienced in using Fork tool).
+                _Experienced in working with WordPress and Shopify.
+                _Experienced in back-end development with PHP, using Symfony and Laravel (beginner).`
+	},
+	{
+		time: '2024 - Now',
+		title: 'Canva Designer',
+		place: 'Freelancer',
+		content: '_Design something cool with canva.'
+	},
+	{
+		time: '2025 - Now',
+		title: 'Front-End Developer',
+		place: 'Freelancer',
+		content: '_Experienced in NextJS (beginner).'
+	}
+];
+
+export const skills = [
+	'html',
+	'css',
+	'scss',
+	'javascript',
+	'jQuery',
+	'reactjs',
+	'nextjs',
+	'wordpress',
+	'shopify',
+	'php',
+	'mysql',
+	'git',
+	'github',
+	'canva'
+];
