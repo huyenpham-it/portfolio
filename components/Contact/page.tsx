2f228e095518f5b21cb9e9ab281e907a1d388615
Huyen Pham <huyen.pham@ascension6.com.au>
2025-07-11 16:33:52 +0700

    update contact page

--- components/Contact/page.tsx
+++ components/Contact/page.tsx
@@ -4,37 +4,46 @@ import styles from './page.module.scss';
 
 export const Contact = () => {
 	return (
-		<div id="contact" className="global-font-color section-separator flex flex-col items-center p-10 lg:p-20">
-			<h2 className="bg-gradient-to-r from-(--red-color) to-(--yellow-color) bg-clip-text text-transparent font-bold uppercase w-fit mb-4">
-				Contact
-			</h2>
-			<div className={`text-transparent text-5xl font-bold mb-10 ${styles.textBorder}`}>Contact me!</div>
-
-			<div>I am available for freelance work.</div>
-			<div>Please contact me via email or social media.</div>
-			<Link
-				href="mailto:dieuhuyenpham.it@gmail.com"
-				className="pink-color text-2xl font-semibold mt-5 mb-10 underline decoration-dotted underline-offset-8"
+		<div
+			id="contact"
+			className={`global-font-color section-separator p-5 sm:p-10 lg:p-20 scroll-mt-20 lg:scroll-mt-10 ${styles.contact}`}
+		>
+			<div
+				className={`max-w-[600px] flex flex-col items-center justify-center py-10 sm:p-20 m-auto ${styles.contactCard}`}
 			>
-				dieuhuyenpham.it@gmail.com
-			</Link>
+				<h2 className="bg-gradient-to-r from-(--red-color) to-(--yellow-color) bg-clip-text text-transparent font-bold uppercase w-fit mb-4">
+					Contact
+				</h2>
+				<div className={`text-transparent text-3xl sm:text-5xl font-bold mb-10 ${styles.textBorder}`}>
+					Contact me!
+				</div>
 
-			<div className="flex gap-8 red-color">
-				<MyCard className="p-4 hover:scale-110 duration-300 ease-in-out">
+				<div className="text-center">I am available for freelance work.</div>
+				<div className="text-center">Please contact me via email or social media.</div>
+				<Link
+					href="mailto:dieuhuyenpham.it@gmail.com"
+					className="pink-color text-lg sm:text-2xl font-semibold mt-5 mb-10 underline decoration-dotted underline-offset-8"
+				>
+					dieuhuyenpham.it@gmail.com
+				</Link>
+
+				<div className="flex gap-8 red-color">
 					<Link href="https://www.facebook.com/dieuhuyen.pham.1997">
-						<Facebook />
+						<MyCard className="p-4 hover:scale-110 duration-300 ease-in-out">
+							<Facebook />
+						</MyCard>
 					</Link>
-				</MyCard>
-				<MyCard className="p-4 hover:scale-110 duration-300 ease-in-out">
 					<Link href="https://www.linkedin.com/in/dieuhuyenpham13091997/">
-						<Linkedin />
+						<MyCard className="p-4 hover:scale-110 duration-300 ease-in-out">
+							<Linkedin />
+						</MyCard>
 					</Link>
-				</MyCard>
-				<MyCard className="p-4 hover:scale-110 duration-300 ease-in-out">
 					<Link href="https://www.instagram.com/cay_thong_nho/">
-						<Instagram />
+						<MyCard className="p-4 hover:scale-110 duration-300 ease-in-out">
+							<Instagram />
+						</MyCard>
 					</Link>
-				</MyCard>
+				</div>
 			</div>
 		</div>
 	);
