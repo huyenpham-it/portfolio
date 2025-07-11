a78b6de82f26feb4fe838998d395a11bed2165fe
Huyen Pham <huyen.pham@ascension6.com.au>
2025-07-11 16:39:29 +0700

    add lazy for all components

--- app/page.tsx
+++ app/page.tsx
@@ -1,71 +1,76 @@
-import { Contact, Features, Footer, IconArrowTop, MyCard, Portfolio, PseudoCorner, Resume } from '@/components/page';
+import {
+	Contact,
+	Features,
+	Footer,
+	HeaderBar,
+	Intro,
+	LazyShow,
+	MyMenu,
+	Portfolio,
+	PseudoCorner,
+	Resume,
+	ScrollToTopButton
+} from '@/components/page';
+import { componentPage } from '@/utils/constants';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
 	return (
 		<div className="w-full h-full bg-background">
-			<header className="w-full h-screen flex justify-center items-center p-10 lg:px-20">
+			<header className="header w-full h-[500px] sm:h-screen flex justify-center items-center p-5 sm:p-10 lg:px-20">
+				<HeaderBar />
 				<div className="w-full h-full relative">
 					<Image
 						src="/avatar.jpg"
 						alt="banner"
 						width={2000}
 						height={1000}
-						className="rounded-[40px]"
+						className="avatar rounded-[40px]"
 						priority
 					/>
 
-					<div className="absolute top-0 right-0 w-[500px] h-[60px]">
+					<div className="absolute top-0 right-0 w-[70px] md:w-[500px] h-[70px] md:h-[60px]">
 						<div className="relative w-full h-full">
-							<div className="absolute left-0 top-0 right-0 bottom-0 w-full h-full bg-background flex justify-center items-center rounded-bl-[40px]">
-								<Link
-									href="#features"
-									className="relative bg-gradient-to-r from-(--red-color) to-(--yellow-color) hover:-translate-y-1 duration-300 ease-in-out bg-clip-text text-xl font-regular text-transparent p-2"
-								>
-									Features
-								</Link>
-								<Link
-									href="#projects"
-									className="bg-gradient-to-r from-(--red-color) to-(--yellow-color) hover:-translate-y-1 duration-300 ease-in-out bg-clip-text text-xl font-regular text-transparent p-2"
-								>
-									Projects
-								</Link>
-								<Link
-									href="#resume"
-									className="bg-gradient-to-r from-(--red-color) to-(--yellow-color) hover:-translate-y-1 duration-300 ease-in-out bg-clip-text text-xl font-regular text-transparent p-2"
-								>
-									Resume
-								</Link>
-								<Link
-									href="#contact"
-									className="bg-gradient-to-r from-(--red-color) to-(--yellow-color) hover:-translate-y-1 duration-300 ease-in-out bg-clip-text text-xl font-regular text-transparent p-2"
-								>
-									Contact
-								</Link>
+							<div className="absolute left-0 top-0 right-0 bottom-0 w-full h-full bg-background rounded-bl-[40px]">
+								<div className="w-full h-full hidden md:flex justify-center items-center">
+									{componentPage.map((item: any, index: number) => (
+										<Link
+											key={index}
+											href={item.path}
+											className="relative bg-gradient-to-r from-(--red-color) to-(--yellow-color) hover:-translate-y-1 duration-300 ease-in-out bg-clip-text text-xl font-regular text-transparent p-2"
+										>
+											{item.title}
+										</Link>
+									))}
+								</div>
+								<div className="w-full h-full flex justify-end items-start md:hidden">
+									<MyMenu />
+								</div>
 							</div>
-							<div className="absolute top-0 right-[500px] w-[25px] h-[25px]">
+
+							<div className="absolute top-0 right-[70px] md:right-[500px] w-[25px] h-[25px]">
 								<PseudoCorner />
 							</div>
-							<div className="absolute top-[60px] right-0 w-[40px] h-[40px]">
+							<div className="absolute top-[70px] md:top-[60px] right-0 w-[40px] h-[40px]">
 								<PseudoCorner />
 							</div>
 						</div>
 					</div>
 
-					<div className="absolute bottom-0 left-0 w-[380px] h-[150px]">
+					<div className="my-info absolute bottom-0 left-0 w-[250px] sm:w-[380px] h-[100px] sm:h-[150px]">
 						<div className="relative w-full h-full">
 							<div className="absolute left-0 top-0 right-0 bottom-0 w-full h-full bg-background flex justify-start items-center rounded-tr-[40px]">
-								<h2 className="bg-gradient-to-r from-(--red-color) to-(--yellow-color) bg-clip-text text-3xl font-extrabold text-transparent p-3">
+								<h2 className="my-info__title bg-gradient-to-r from-(--red-color) to-(--yellow-color) bg-clip-text text-xl sm:text-3xl font-extrabold text-transparent p-3">
 									Hi, I'm Huyen Pham
 									<br />
 									Front-end Developer
 								</h2>
 							</div>
-							<div className="absolute bottom-[150px] left-0 w-[40px] h-[40px] rotate-180">
+							<div className="pseudo-corner absolute bottom-[100px] sm:bottom-[150px] left-0 w-[40px] h-[40px] rotate-180">
 								<PseudoCorner />
 							</div>
-							<div className="absolute bottom-0 left-[380px] w-[40px] h-[40px] rotate-180">
+							<div className="pseudo-corner absolute bottom-0 left-[250px] sm:left-[380px] w-[40px] h-[40px] rotate-180">
 								<PseudoCorner />
 							</div>
 						</div>
@@ -73,17 +78,31 @@ export default function Home() {
 				</div>
 			</header>
 
-			<Features />
-			<Portfolio />
-			<Resume />
-			<Contact />
-			<Footer />
+			<LazyShow>
+				<Intro />
+			</LazyShow>
 
-			<div className="fixed bottom-10 right-10 global-font-color z-100">
-				<MyCard className="rounded-full p-4 hover:scale-110 duration-300 ease-in-out">
-					<IconArrowTop />
-				</MyCard>
-			</div>
+			<LazyShow>
+				<Features />
+			</LazyShow>
+
+			<LazyShow>
+				<Portfolio />
+			</LazyShow>
+
+			<LazyShow>
+				<Resume />
+			</LazyShow>
+
+			<LazyShow>
+				<Contact />
+			</LazyShow>
+
+			<LazyShow>
+				<Footer />
+			</LazyShow>
+
+			<ScrollToTopButton />
 		</div>
 	);
 }
