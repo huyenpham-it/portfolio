b38b2d70f255b4190da0f6fff216f66ae0ab0865
Huyen Pham <huyen.pham@ascension6.com.au>
2025-07-12 18:50:07 +0700

    uninstall @material-tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,7 @@
 export default {
 	content: [
 		'./app/**/*.{html,js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-		'./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}',
-		'./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}'
+		'./components/**/*.{js,ts,jsx,tsx}'
 	],
 	// safelist: [
 	// 	{
