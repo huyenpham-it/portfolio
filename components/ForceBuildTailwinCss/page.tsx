a8ca02e7b7d22cfadfc68f71b9d42c0f053537bd
Huyen Pham <huyen.pham@ascension6.com.au>
2025-07-16 17:00:36 +0700

    Revert "test"
    This reverts commit 881a52db6c1ae9cc0c2d63eb23deeedb647ab047.

--- components/ForceBuildTailwinCss/page.tsx
+++ components/ForceBuildTailwinCss/page.tsx
@@ -1,7 +1,7 @@
 const ForceBuildTailwinCss = () => {
 	return (
 		<>
-			<div className="grid place-items-center fixed w-screen h-screen backdrop-blur-sm bg-white/30"></div>
+			<div className="grid place-items-center fixed w-screen h-screen backdrop-blur-sm"></div>
 			<div className="relative m-4 shadow-2xl antialiased leading-relaxed md:w-5/6 2xl:w-3/4 min-w-[95%] md:min-w-[83.333333%] 2xl:min-w-[75%] max-w-[95%] md:max-w-[83.333333%] 2xl:max-w-[75%]"></div>
 		</>
 	);
